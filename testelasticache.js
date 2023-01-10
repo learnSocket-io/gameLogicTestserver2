f615c49764d69c7338de0258a8a312d797b05770
yesk31 <yesk31@naver.com>
2023-01-10 21:42:08 +0900

    fix: elasticache

--- testelasticache.js
+++ testelasticache.js
@@ -2,7 +2,10 @@
 
 const { createClient } = require("redis");
 
-const client = createClient();
+//EC2에서 연결할때는 경로 분기를 바꿔줘야한다.
+const client = createClient({
+  url: process.env.REDIS_HOST,
+});
 
 console.log("Before client.connect()...");
 
@@ -10,13 +13,20 @@ console.log("Before client.connect()...");
 const connectClient = async () => {
   return await client.connect();
 };
-const connectPromise = connectClient();
-//연결 확인
-console.log("After client.connect()...");
+connectClient()
+  .then(() => {
+    //연결 확인
+    console.log("After client.connect()...");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
 
-//  console.log(
-//    `client.isOpen: ${client.isOpen}, client.isReady: ${client.isReady}`
-//  );
+
+
+console.log(
+  `client.isOpen: ${client.isOpen}, client.isReady: ${client.isReady}`
+);
 
 //await connectPromise;
 // console.log("Afer connectPromise has resolved...");
